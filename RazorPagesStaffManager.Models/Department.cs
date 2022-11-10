e22f23ccbc86cdf16ac2fe2fb9c41fea96f2f72d
Samitova Vika <samitova@gmail.com>
2022-11-10 15:00:27 +0100

    Initial project configuration was created 1. Added Client part project 2. Added Model part project 3. Added Services part project

---
+++ RazorPagesStaffManager.Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesStaffManager.Models
+{
+    public enum Department
+    { 
+        None,
+        HR,
+        IT,
+        Payroll
+    }
+}
